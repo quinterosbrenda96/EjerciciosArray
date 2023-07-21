b5b3617e6ff2ac0835695a3a926a9a58983ebe52
Brenda Abigail Quinteros <quinterosbrenda96@gmail.com>
2023-07-21 18:10:50 -0300

    Update filtrarRangoEnLugar.js

--- filtrarRangoEnLugar.js
+++ filtrarRangoEnLugar.js
@@ -1,8 +1,7 @@
 function filterRangeOnPlace(arr, a, b) {
   for (let i = 0; i < arr.length; i ++) {
-  if  (a => arr[i] && arr[i] >= b) {
-  arr.splice(i, 1)
+    if  (a => arr[i] && arr[i] >= b) {
+      arr.splice(i, 1)
+    }
   }
-  }
-
 }
