98409448c487f6b95fef1f1ae03f78afe6240802
Brenda Abigail Quinteros <quinterosbrenda96@gmail.com>
2023-07-24 17:44:00 -0300

    filtrarunRango.js

--- filtrarunRango.js
+++ filtrarunRango.js
@@ -1,3 +1,9 @@
+//Escribir una función filterRange(arr, a, b) que obtenga un array arr
+// busque los elementos con valor mayor o igual a a y menor o igual a b y devuelva un array con los resultados.
+
+
+
+
 function filterRangeInPlace (arr, a, b) {
   return arr.filter((item) => {
     return item >= a && item <= b
