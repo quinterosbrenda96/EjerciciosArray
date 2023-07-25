bdb6fa7ba6a10f3116ef9ac5ae27ab7bbc7bd4b4
Brenda Abigail Quinteros <quinterosbrenda96@gmail.com>
2023-07-25 16:54:00 -0300

    filtrarRangoEnLugar.js

--- filtrarRangoEnLugar.js
+++ filtrarRangoEnLugar.js
@@ -1,3 +1,11 @@
+// Escribir una función filterRangeInPlace(arr, a, b) que obtenga un array arr y remueva del mismo todos los valores
+// excepto aquellos que se encuentran entre a y b. El test es: a ≤ arr[i] ≤ b.
+
+La función solo debe modificar el array. No debe devolver nada.
+
+
+
+
 function filterRangeOnPlace(arr, a, b) {
   for (let i = 0; i < arr.length; i ++) {
     if  (a => arr[i] && arr[i] >= b) {
